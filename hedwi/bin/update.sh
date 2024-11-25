c89e66a8f4ec9217257e66c0eaf01d6e38baf1b7
solos <lxl1217@gmail.com>
2024-11-25 14:19:40 +0800

    read .env before run
    read .env before run

--- hedwi/bin/update.sh
+++ hedwi/bin/update.sh
@@ -1,10 +1,16 @@
- docker pull "$PROXY"hedwi/hedwi-inbox:latest
- docker pull "$PROXY"hedwi/hedwi-meetserver:latest
- docker pull "$PROXY"hedwi/hedwi-office-api:latest
- docker pull "$PROXY"hedwi/hedwi-hub:latest
- docker pull "$PROXY"hedwi/hedwi-smtp:latest
- docker pull "$PROXY"hedwi/hedwi-imap:latest
- docker pull "$PROXY"hedwi/hedwi-task:latest
- docker pull "$PROXY"hedwi/hedwi-search:latest
- docker pull "$PROXY"hedwi/hedwi-filter:latest
- docker pull "$PROXY"hedwi/hedwi-mqtt:latest
+#!/bin/bash
+
+set -a # 将所有变量自动导出
+source ./.env
+set +a
+
+docker pull "$PROXY"hedwi/hedwi-inbox:latest
+docker pull "$PROXY"hedwi/hedwi-meetserver:latest
+docker pull "$PROXY"hedwi/hedwi-office-api:latest
+docker pull "$PROXY"hedwi/hedwi-hub:latest
+docker pull "$PROXY"hedwi/hedwi-smtp:latest
+docker pull "$PROXY"hedwi/hedwi-imap:latest
+docker pull "$PROXY"hedwi/hedwi-task:latest
+docker pull "$PROXY"hedwi/hedwi-search:latest
+docker pull "$PROXY"hedwi/hedwi-filter:latest
+docker pull "$PROXY"hedwi/hedwi-mqtt:latest
