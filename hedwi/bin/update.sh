36855aafd8c9a186c2e5ac6d23a5b38f711e6aa2
solos <lxl1217@gmail.com>
2024-12-01 16:06:18 +0800

    fix update.sh
    fix update.sh

--- hedwi/bin/update.sh
+++ hedwi/bin/update.sh
@@ -4,13 +4,13 @@ set -a # 将所有变量自动导出
 source ./.env
 set +a
 
-docker pull "$PROXY"hedwi/hedwi-inbox:latest
-docker pull "$PROXY"hedwi/hedwi-meetserver:latest
-docker pull "$PROXY"hedwi/hedwi-office-api:latest
-docker pull "$PROXY"hedwi/hedwi-hub:latest
-docker pull "$PROXY"hedwi/hedwi-smtp:latest
-docker pull "$PROXY"hedwi/hedwi-imap:latest
-docker pull "$PROXY"hedwi/hedwi-task:latest
-docker pull "$PROXY"hedwi/hedwi-search:latest
-docker pull "$PROXY"hedwi/hedwi-filter:latest
-docker pull "$PROXY"hedwi/hedwi-mqtt:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-inbox:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-meetserver:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-office-api:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-hub:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-smtp:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-imap:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-task:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-search:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-filter:latest
+docker pull "$DOCKER_REGISTRY"hedwi/hedwi-mqtt:latest
